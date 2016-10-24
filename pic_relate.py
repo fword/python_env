a92210048b26df488c4e51c5fe801ac54456d189
fword <jieao111@126.com>
2016-10-24 19:22:33 +0800

    Add files via upload

--- pic_relate.py
+++ pic_relate.py
@@ -17,3 +17,49 @@ def del_pic_trash(mydir):
             file=os.path.join(root,fileone)
             if not _is_real_pic(file):
                 os.remove(file)
+def noisy(noise_typ,image):
+    import cv2
+    import numpy as np
+    import random
+    if noise_typ == "gauss":
+        row,col,ch= image.shape
+        mean = 0
+        var = 0.1
+        sigma = var**0.5
+        gauss = np.random.normal(mean,sigma,(row,col,ch))
+        gauss = gauss.reshape(row,col,ch)
+        noisy = image + gauss
+        return noisy
+    elif noise_typ == "sp":
+        row,col,ch = image.shape
+        s_vs_p = 0.5
+        amount = 0.004
+        out = image
+        # Salt mode
+        num_salt = np.ceil(amount * image.size * s_vs_p)
+        coords = [np.random.randint(0, i - 1, int(num_salt)) for i in image.shape]
+        out[coords] = 1
+        # Pepper mode
+        num_pepper = np.ceil(amount* image.size * (1. - s_vs_p))
+        coords = [np.random.randint(0, i - 1, int(num_pepper)) for i in image.shape]
+        out[coords] = 0
+        return out
+    elif noise_typ == "gus":
+        gaussian_noise = image.copy()
+        cv2.randn(gaussian_noise,128,30)
+        out=gaussian_noise+image
+        return out
+    elif noise_typ == "gusblur":
+        out=cv2.GaussianBlur(image,(5,5),1.5)
+        return out
+    elif noise_typ == "shuiyin":
+        block=50
+        shuiyin = np.ones((block,block,3),np.uint8)
+        row,col,ch = image.shape
+        hl=random.randint(0,row-block)
+        wl=random.randint(0,col-block)
+        image[hl:hl+block,wl:wl+block,:]=shuiyin
+        return image
+    else:
+        return image
+
