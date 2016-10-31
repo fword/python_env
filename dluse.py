5a11c429e1089a99fc1541f4d0fa53848c26aea4
fword <jieao111@126.com>
2016-10-31 16:20:38 +0800

    Add files via upload

--- dluse.py
+++ dluse.py
@@ -1,11 +1,22 @@
 import sys,os
-import urllib
+import urllib,urllib2
 import traceback
 def quchong(mylist):
     aa=set()
     for item in mylist:
         aa.add(item.strip())
     return list(aa)
+def filediff(file1,file2):
+    aa=set()
+    bb=set()
+    for item in open(file1):
+        aa.add(item.strip())
+    for item in open(file2):
+        bb.add(item.strip())
+    if len(aa)>len(bb):
+        return list(aa-bb)
+    else:
+        return list(bb-aa)
 def _download(filename,url):
     for i in range(3):
         try:
@@ -39,20 +50,24 @@ def muti_xiecheng_download(filename):
     gevent.spawn(boss,filename).join()
     threads = [gevent.spawn(worker,i) for i in range(100)]
     gevent.joinall(threads)
-def _is_real_pic(filename):
-    import cv2
-    try:
-        ret=cv2.imread(filename)
-        if ret is None: 
-            return 0
-        else:
+
+headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.72 Safari/537.36'}
+def urlopen(url,trytimes=3,timeout=5,headers=headers):
+    for i in range(trytimes):
+        try:
+            request = urllib2.Request(url=url,headers=headers)
+            response = urllib2.urlopen(request, timeout=timeout)
+            return response.read()
+        except:
+            pass
+def urlstore(url,filename,trytimes=3,timeout=5):
+    import socket
+    socket.setdefaulttimeout(timeout)
+    for i in range(trytimes):
+        try:
+            urllib.urlretrieve(url,filename)
             return 1
-    except:
-        return 0
+        except:
+            pass
     return 0
-def del_pic_trash(mydir):
-    for root,dirfile,filelist in os.walk(mydir):
-        for fileone in filelist:
-            file=os.path.join(root,fileone)
-            if not _is_real_pic(file):
-                os.remove(file)
+
